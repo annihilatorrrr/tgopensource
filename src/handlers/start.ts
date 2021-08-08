a84c5eee80c279ed08f8e68a3a64ed0c6a162ce2
Roj Serbest <roj@rojser.best>
2021-08-08 14:30:07 +0300

    a

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -8,7 +8,7 @@ composer.command("start", async (ctx) => {
   await ctx.reply(
     `👋 <i>Hello!</i>
   
-You can use me <b>submit your Telegram bot to be added to <i>@TGOpenSource</></>.
+You can use me <b>submit your Telegram project to be added to <i>@TGOpenSource</></>.
   
 👨‍💻 Use /new or the button below to <b>make a new submittion</>.`,
     {
